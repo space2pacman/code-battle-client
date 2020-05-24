4316cd94761b783c75f9748d100be02f17029f0d
space2pacman <space2pacman@gmail.com>
2020-05-24 21:58:25 +0300

    add filter to tasks page, add data to TaskCard

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,8 +1,12 @@
 <template>
 	<router-link to="/task/0" class="card mb-3 text-decoration-none text-muted">
 		<div class="card-body d-flex justify-content-between align-items-center">
-			<span>Перевернуть строку</span>
-			<span class="badge badge-danger p-2 font-weight-normal">Не выполнено</span>
+			<div class="mr-auto">
+				<div class="mb-1">Перевернуть строку</div>
+				<small class="text-muted">Легкий | Решено: 8086 | Комментариев: 324</small>
+			</div>
+			<span class="badge badge-light p-2 font-weight-normal ml-3">Автор: space2pacman</span>
+			<span class="badge badge-danger p-2 font-weight-normal ml-3">Не выполнено</span>
 		</div>
 	</router-link>
 </template>
--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -1,7 +1,66 @@
 <template>
 	<div>
 		<h4 class="mb-3">Задания</h4>
-		<TaskCard />
-		<TaskCard />
+		<div class="row">
+			<div class="col-9">
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+			</div>
+			<div class="col-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="mb-4">Фильтр</h5>
+						<div class="mb-4">
+							<h6>Уровень</h6>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck1">
+								<label class="custom-control-label" for="customCheck1">Легкий</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck2">
+								<label class="custom-control-label" for="customCheck2">Средений</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck3">
+								<label class="custom-control-label" for="customCheck3">Сложный</label>
+							</div>
+						</div>
+						<div class="mb-4">
+							<h6>Компании</h6>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck4">
+								<label class="custom-control-label" for="customCheck4">Github</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck5">
+								<label class="custom-control-label" for="customCheck5">Google</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck6">
+								<label class="custom-control-label" for="customCheck6">Yandex</label>
+							</div>
+						</div>
+						<div class="mb-4">
+							<h6>Теги</h6>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck7">
+								<label class="custom-control-label" for="customCheck7">Javasript</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck8">
+								<label class="custom-control-label" for="customCheck8">PHP</label>
+							</div>
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customCheck9">
+								<label class="custom-control-label" for="customCheck9">Vue.js</label>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
