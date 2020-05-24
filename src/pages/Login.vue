82499b3d52f88a418ca2b7b4790e7523ac1fcf47
space2pacman <space2pacman@gmail.com>
2020-05-25 00:51:07 +0300

    add (Login, Registration) template

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,22 @@
 <template>
-	<div>
-		Login
+	<div class="w-25 mx-auto">
+		<h4 class="card-title mb-3 text-center">Вход</h4>
+		<div class="card">
+			<div class="card-body">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="E-mail">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="Пароль">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">Войти</button>
+				</div>
+				<div class="text-center">
+					<span>Ещё нет аккаунта?</span> 
+					<router-link to="/registration">Зарегистрируйтесь</router-link>
+				</div> 
+			</div>
+		</div>
 	</div>
 </template>
--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -1,5 +1,25 @@
 <template>
-	<div>
-		Registration
+	<div class="w-25 mx-auto">
+		<h4 class="card-title mb-3 text-center">Регистрация</h4>
+		<div class="card">
+			<div class="card-body">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Никнейм">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="E-mail">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="Пароль">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">Зарегистрироваться</button>
+				</div>
+				<div class="text-center">
+					<span>Уже зарегистрированы?</span> 
+					<router-link to="/login">Войдите</router-link>
+				</div> 
+			</div>
+		</div>
 	</div>
 </template>
