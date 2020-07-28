c1eaba5240cf9747e2391e6e8a6e2cd9b11e4caf
space2pacman <space2pacman@gmail.com>
2020-07-28 21:05:05 +0300

    add TaskFields

---
+++ src/components/TaskFields.vue
@@ -0,0 +1,93 @@
+<template>
+	<div class="mt-3">
+		<input type="text" class="form-control mb-3" placeholder="Название" v-model="data.caption">
+		<textarea class="form-control mb-3" placeholder="Описание" v-model="data.description"></textarea>
+		<input type="number" class="form-control mb-3" placeholder="Уровень" v-model="data.level">
+		<input type="text" class="form-control mb-3" placeholder="Язык" v-model="data.language">
+		<input type="text" class="form-control mb-3" placeholder="Имя функции" v-model="data.func.name">
+		<div class="mb-1">Тесты</div>
+		<div v-for="(test, index) in data.tests" class="d-flex mb-3">
+			<input type="text" class="form-control mr-3" placeholder="На вход" v-model="test.input.value">
+			<select class="form-control mr-3" v-model="test.input.type">
+				<option>String</option>
+				<option>Number</option>
+				<option>Boolean</option>
+				<option>Object</option>
+				<option>Array</option>
+			</select>
+			<input type="text" class="form-control mr-3" placeholder="На выход" v-model="test.output.value">
+			<select class="form-control mr-3" v-model="test.output.type">
+				<option>String</option>
+				<option>Number</option>
+				<option>Boolean</option>
+				<option>Object</option>
+				<option>Array</option>
+			</select>
+			<button type="button" class="btn btn-danger float-right" @click="removeFields(index)">
+				<i class="fas fa-minus"></i>
+			</button>
+		</div>
+		<button type="button" class="btn btn-success float-right" @click="addFields">
+			<i class="fas fa-plus"></i>
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			data: {
+				caption: null,
+				description: null,
+				level: null,
+				language: null,
+				func: {
+					name: null
+				},
+				tests: [
+					{
+						input: {
+							value: null,
+							type: null
+						},
+						output: {
+							value: null,
+							type: null
+						}
+					}
+				]
+			}
+		}
+	},
+	methods: {
+		removeFields(index) {
+			if(this.tests.length > 1) {
+				this.tests.splice(index, 1);
+			}
+		},
+		addFields() {
+			let fields = {
+				input: {
+					value: null,
+					type: null
+				},
+				output: {
+					value: null,
+					type: null
+				}
+			}
+
+			this.data.tests.push(fields);
+		}
+	},
+	watch: {
+		data: {
+			deep: true,
+			handler() {
+				this.$emit("data", this.data);
+			}
+		}
+	}
+}
+</script>
--- src/pages/TaskAdd.vue
+++ src/pages/TaskAdd.vue
@@ -3,40 +3,17 @@
 		<h1 class="mb-4">Новая задача</h1>
 		<div class="row">
 			<div class="col">
-				<input type="text" class="form-control mb-3" placeholder="Название" v-model="data.caption">
-				<textarea class="form-control mb-3" placeholder="Описание" v-model="data.description"></textarea>
-				<input type="number" class="form-control mb-3" placeholder="Уровень" v-model="data.level">
-				<input type="text" class="form-control mb-3" placeholder="Язык" v-model="data.language">
-				<input type="text" class="form-control mb-3" placeholder="Имя функции" v-model="data.func.name">
-				<div class="mb-1">Тесты</div>
-				<div v-for="(test, index) in data.tests" class="d-flex mb-3">
-					<input type="text" class="form-control mr-3" placeholder="На вход" v-model="test.input.value">
-					<select class="form-control mr-3" v-model="test.input.type">
-						<option>String</option>
-						<option>Number</option>
-						<option>Boolean</option>
-						<option>Object</option>
-						<option>Array</option>
-					</select>
-					<input type="text" class="form-control mr-3" placeholder="На выход" v-model="test.output.value">
-					<select class="form-control mr-3" v-model="test.output.type">
-						<option>String</option>
-						<option>Number</option>
-						<option>Boolean</option>
-						<option>Object</option>
-						<option>Array</option>
-					</select>
-					<button type="button" class="btn btn-danger float-right" @click="removeFields(index)">
-						<i class="fas fa-minus"></i>
-					</button>
-				</div>
-				<button type="button" class="btn btn-success float-right" @click="addFields">
-					<i class="fas fa-plus"></i>
-				</button>
+				<ul class="nav nav-tabs">
+					<li v-for="tab in tabs.list" class="nav-item">
+						<a class="nav-link" :class="{ 'active': tab.type === tabs.active }" href="#" @click.prevent="switchTab(tab.type)">{{ tab.caption }}</a>
+					</li>
+				</ul>
+				<task-fields v-show="tabs.active === 'description'" @data="onTaskFields" />
+				<result v-show="tabs.active === 'result'" />
 				<!-- <Notice :text="'test'" /> -->
 			</div>
 			<div class="col">
-				<Ace lang="javascript" theme="monokai" height="400" v-model="data.func.body" />
+				<Ace lang="javascript" theme="monokai" height="400" v-model="func.body" />
 				<button type="button" class="btn btn-success float-right ml-3 mt-3" @click="checkTask">Проверка</button>
 				<button type="button" class="btn btn-success float-right mt-3" @click="addTask" :class="{ 'disabled' : !completed }">Добавить</button>
 			</div>
@@ -46,6 +23,8 @@
 
 <script>
 //import Notice from "@/components/Notice";
+import TaskFields from "@/components/TaskFields";
+import Result from "@/components/Result";
 import Ace from "vue2-ace-editor";
 import "brace/mode/javascript";
 import "brace/theme/monokai";
@@ -53,56 +32,38 @@ import "brace/theme/monokai";
 export default {
 	data() {
 		return {
-			data: {
-				caption: null,
-				description: null,
-				level: null,
-				language: null,
-				func: {
-					name: null,
-					body: null
-				},
-				tests: [
+			tabs: {
+				list: [
 					{
-						input: {
-							value: null,
-							type: null
-						},
-						output: {
-							value: null,
-							type: null
-						}
+						caption: "Описание",
+						type: "description"
+					},
+					{
+						caption: "Результат проверки",
+						type: "result"
 					}
-				]
+				],
+				active: "description"
 			},
+			func: {
+				body: null
+			},
+			fields: null,
 			completed: false
 		}
 	},
 	methods: {
-		removeFields(index) {
-			if(this.tests.length > 1) {
-				this.tests.splice(index, 1);
-			}
-		},
-		addFields() {
-			let fields = {
-				input: {
-					value: null,
-					type: null
-				},
-				output: {
-					value: null,
-					type: null
-				}
-			}
-
-			this.data.tests.push(fields);
+		switchTab(tab) {
+			this.tabs.active = tab;
 		},
 		checkTask() {
 			let payload = {
 				data: {
-					func: this.data.func,
-					tests: this.data.tests
+					func: {
+						name: this.fields.func.name,
+						body: this.func.body
+					},
+					tests: this.fields.tests
 				}
 			}
 
@@ -114,10 +75,15 @@ export default {
 			}
 			
 			this.send("task/add", payload);
+		},
+		onTaskFields(data) {
+			this.fields = data;
 		}
 	},
 	components: {
 		//Notice,
+		TaskFields,
+		Result,
 		Ace
 	}
 }
