ebc07384bf6b6c36588bcba972aa9da3d3f6f172
space2pacman <space2pacman@gmail.com>
2020-08-11 21:04:32 +0300

    add mobile dropdown nav

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="mb-4">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <router-link class="navbar-brand" to="/">Code battle</router-link>
                 <div class="collapse navbar-collapse">
@@ -29,7 +29,7 @@
                                     <img :src="userpic" class="align-sub" width="20" alt="avatar">
                                     <span class="ml-2 mr-1">{{ getAuthUserName }}</span>
                                 </a>
-                                <div slot="dropdown" @click="dropdown = false">
+                                <div slot="dropdown">
                                     <router-link :to="`/user/${getAuthUserName}`" class="dropdown-item">Профиль</router-link>
                                     <router-link to="/task/add/" class="dropdown-item" v-if="checkAccessLevel(100)">Добавить задачу</router-link>
                                     <router-link :to="`/user/${getAuthUserName}/settings`" class="dropdown-item">Настройки</router-link>
@@ -47,9 +47,34 @@
         <transition name="mobile-nav">
             <div class="bg-light" v-if="showMobileMenu">
                 <div class="container text-right p-0 d-lg-none">
-                    <div>items</div>
-                    <div>items</div>
-                    <div>items</div>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <router-link to="/" class="nav-link">Главная</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/tasks" class="nav-link">Задачи</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/registration" class="nav-link" v-if="!isAuthorized">Регистрация</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/login" class="nav-link" v-if="!isAuthorized">Войти</router-link>
+                        </li>
+                        <li class="nav-item dropdown" v-if="isAuthorized">
+                            <a class="nav-link dropdown-toggle" href="#" @click.prevent="dropdown = !dropdown">
+                                <img :src="userpic" class="align-sub" width="20" alt="avatar">
+                                <span class="ml-2 mr-1">{{ getAuthUserName }}</span>
+                            </a>
+                            <transition name="mobile-nav">
+                                <div v-if="dropdown" class="dropdown__mobile">
+                                    <router-link :to="`/user/${getAuthUserName}`" class="dropdown-item">Профиль</router-link>
+                                    <router-link to="/task/add/" class="dropdown-item" v-if="checkAccessLevel(100)">Добавить задачу</router-link>
+                                    <router-link :to="`/user/${getAuthUserName}/settings`" class="dropdown-item">Настройки</router-link>
+                                    <router-link to="/logout" class="dropdown-item">Выйти</router-link>
+                                </div>
+                            </transition>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </transition>
@@ -78,6 +103,12 @@ export default {
             this.showMobileMenu = !this.showMobileMenu;
         }
     },
+    watch: {
+        $route() {
+            this.dropdown = false;
+            this.showMobileMenu = false;
+        }
+    },
     mounted() {
         this.$store.subscribe(mutation => {
             if(mutation.type === "user/auth") {
@@ -96,10 +127,17 @@ export default {
     vertical-align: sub;
 }
 
+/* dropdown */
 .dropdown-menu {
     top: 38px !important;
 }
-/* modile menu */
+
+.dropdown__modile {
+    border-top: 1px solid #dcdcdc;
+    padding: 8px 0px;
+}
+
+/* modile menu animate */
 .mobile-nav-enter-active {
    transition-duration: 0.3s;
    transition-timing-function: ease-in;
@@ -120,14 +158,16 @@ export default {
    max-height: 0;
 }
 
-/* dropdown menu */
+/* dropdown menu animate */
 .translate-fade-down-enter-active, .translate-fade-down-leave-active {
     transition: all 250ms;
     transition-timing-function: cubic-bezier(.53,2,.36,.85);
 }
+
 .translate-fade-down-enter, .translate-fade-down-leave-active {
     opacity: 0;
 }
+
 .translate-fade-down-enter, .translate-fade-down-leave-to {
     position: absolute;
 }
@@ -135,6 +175,7 @@ export default {
 .translate-fade-down-enter {
     transform: translateY(-10px);
 }
+
 .translate-fade-down-leave-active {
     transform: translateY(10px);
 }
