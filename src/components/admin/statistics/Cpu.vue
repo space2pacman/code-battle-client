7ce82787fa5545cfe8c0af62d2dc327ce20533a7
space2pacman <space2pacman@gmail.com>
2020-09-25 00:17:19 +0300

    add core info, show info by first core

--- src/components/admin/statistics/Cpu.vue
+++ src/components/admin/statistics/Cpu.vue
@@ -1,5 +1,73 @@
 <template>
-	<div>
-		Cpu
+	<div class="card">
+		<div class="card-body">
+			<div class="card">
+				<div class="card-header">Intel(R) Core(TM) i3-7020U CPU @ 2.30GHz</div>
+				<div class="card-body">
+					<Chart :data="data" />
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
+
+<script>
+import Chart from "@/components/Chart";
+
+export default {
+	data() {
+		return {
+			timer: null,
+			data: {
+				labels: [],
+				datasets: [
+					{
+						label: "USER",
+						backgroundColor: "#9fb2d4",
+						data: []
+					},
+					{
+						label: "SYSTEM",
+						backgroundColor: "#65709e",
+						data: []
+					},
+					{
+						label: "TOTAL",
+						backgroundColor: "#2c4c6c",
+						data: []
+					}
+				]
+			}
+		}
+	},
+	methods: {
+		addData(data) {
+			this.data.labels.push(this.getDate());
+			this.data.datasets[0].data.push(data[0].load.user);
+			this.data.datasets[1].data.push(data[0].load.system);
+			this.data.datasets[2].data.push(data[0].load.total);
+		},
+		requestData() {
+			this.receive("system/cpu").then(response => {
+				if(response.status === "success") {
+					this.addData(response.data);
+				}
+			});
+		}
+	},
+	mounted() {
+		this.requestData();
+		
+		clearInterval(this.timer);
+		this.timer = setInterval(() => {
+			this.requestData();
+		}, 5000)
+	},
+	beforeDestroy() {
+		clearInterval(this.timer);
+	},
+	components: {
+		Chart
+	}
+}
+</script>
